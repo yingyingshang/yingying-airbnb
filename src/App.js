37c60eceb20b75d3dcc21fd6505228e4374d8b8a
Brita Shang <brita.shang@activenetwork.com>
2022-10-23 10:12:03 +0800

    update the text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <h1>Testing github pages with react and github actions</h1>
         </p>
         <a
           className="App-link"
