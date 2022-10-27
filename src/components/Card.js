c60e623e309d9873778ac935be0280d699c46759
Lei Dong <lei.dong@rea-group.com>
2022-10-27 19:55:53 +0800

    Update images

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,11 +1,13 @@
 import React from "react";
+import Swin from "../images/swim.png"
+import Star from "../images/star.png"
 
 export default function Card() {
     return (
         <div>
-            <img src="../images/swim.png" className="card--image"/>
+            <img src={Swin} className="card--image" />
             <div className="card--stats">
-                <img src="../images/star.pg" className="card--star" />
+                <img src={Star} className="card--star" />
                 <span>5.0</span>
                 <span className="gray">(6) • </span>
                 <span className="gray">USA</span>
