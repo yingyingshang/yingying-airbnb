45b713799d8413cbfeb89c70477d4f7db1b7949e
Lei Dong <lei.dong@rea-group.com>
2022-10-27 19:00:57 +0800

    Fix image display

---
+++ src/components/Navbar.js
@@ -0,0 +1,10 @@
+import React from "react"
+import Logo from '../images/airbnb-logo.png'
+
+export default function Navbar() {
+    return (
+        <nav>
+            <img src={Logo} alt="logo" />
+        </nav>
+    )
+}
