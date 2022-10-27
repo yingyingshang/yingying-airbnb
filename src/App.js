88f13ab13e4f4631133a90454befa3cbba9a9b15
Lei Dong <lei.dong@rea-group.com>
2022-10-27 19:00:57 +0800

    Fix image display

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Card from "./components/Card"
+
+/*
+Challenge: Build the Navbar component.
+Check the Figma file for the design specifics.
+*/
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          <h1>Testing github pages with react and github actions</h1>
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar />
+      <Hero />
+      <Card />
     </div>
   );
 }
