d6cbe26b5200748d9c48317d2f9396d226d2e56c
Brita Shang <brita.shang@activenetwork.com>
2022-11-06 10:04:56 +0800

    add first props

--- src/App.js
+++ src/App.js
@@ -1,20 +1,63 @@
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Card from "./components/Card"
+import Swin from "./images/swim.png"
+import Star from "./images/star.png"
 
 /*
 Challenge: Build the Navbar component.
 Check the Figma file for the design specifics.
 */
 
-function App() {
+export default function App(){
   return (
     <div>
       <Navbar />
       <Hero />
-      <Card />
+      <Card 
+          img = {Swin}
+          rating = "5.0"
+          reviewCount = {6}
+          country = "USA"
+          title = "Life Lessons with Katie Zafers"
+          price = {136}
+      />
     </div>
   );
 }
 
-export default App;
+
+
+
+
+
+
+
+
+
+
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Yingying Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
--- src/components/Card.js
+++ src/components/Card.js
@@ -2,18 +2,19 @@ import React from "react";
 import Swin from "../images/swim.png"
 import Star from "../images/star.png"
 
-export default function Card() {
+export default function Card(props) {
+    console.log(props)
     return (
-        <div>
-            <img src={Swin} className="card--image" alt="siwn" />
+        <div className="Card">
+            <img src={`${props.img}`} className="card--image" alt="siwn" />
             <div className="card--stats">
                 <img src={Star} className="card--star" alt="star" />
-                <span>5.0</span>
-                <span className="gray">(6) • </span>
-                <span className="gray">USA</span>
+                <span>{props.rating}</span>
+                <span className="gray">({props.reviewCount}) • </span>
+                <span className="gray">{props.country}</span>
             </div>
-            <p>Life Lessons with Katie Zaferes</p>
-            <p><span className="bold">From $136</span> / person</p>
+            <p>{props.title}</p>
+            <p><span className="bold">From Brita ${props.price}</span> / person</p>
         </div>
     )
 }
