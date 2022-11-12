4456d37f3ddab667c5af4aa604810ca696e8c433
Brita Shang <brita.shang@activenetwork.com>
2022-11-12 11:24:33 +0800

    add css style

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -5,7 +5,7 @@ export default function Hero() {
     return (
         <section className="hero">
             <img src={PhotoGrid} className="hero--photo" alt=""></img>
-            <h1 className="hero--header">Online Experiences</h1>
+            <h1 className="hero--header">Brita Online Experiences</h1>
             <p className="hero--text">Join unique interactive activities led by
                 one-of-a-kind hosts-all without leaving home.</p>
         </section>
