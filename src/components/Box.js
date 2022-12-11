2efd909bc938be786403f179423c2472f4b4039d
Brita Shang <brita.shang@activenetwork.com>
2022-12-11 11:39:17 +0800

    change toggle

--- src/components/Box.js
+++ src/components/Box.js
@@ -1,16 +1,22 @@
 import React from "react";
 import boxes from "./boxesData"
 
-export default function Box(props){
+export default function Box(){
 
     const [squares] = React.useState(boxes)
 
+    const [on, setOn] = React.useState(squares.on)
+
     const styles = {
-        backgroundColor: squares.on ? "#222222" : "#222222"
+        backgroundColor: on ? "#222222" : "transparent"
+    }
+
+    function toggle(){
+        setOn(preOn => !preOn)
     }
 
     const squareElements = squares.map(square => (
-      <div style={styles} className="box" key={square.id} on={square.on}/>
+      <div style={styles} className="box" key={square.id} on={square.on} onClick={toggle}/>
     ))
 
     return (
