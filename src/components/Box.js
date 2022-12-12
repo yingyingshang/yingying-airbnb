ae0de5ae1082aaef391a88b79c7ecdabe86c9b07
Brita Shang <brita.shang@activenetwork.com>
2022-12-12 15:09:56 +0800

    refactoring toggle reset squares map method

--- src/components/Box.js
+++ src/components/Box.js
@@ -9,31 +9,34 @@ export default function Box(){
 
     function toggle(id){
         setSquares(prevSquares => {
-            const newSquares = []
-            for (let i = 0; i < prevSquares.length; i++){
-                const currentSqaure = prevSquares[i]
-                if(currentSqaure.id === id){
-                    const updatedSqaure = {
-                        ...currentSqaure,
-                        on: !currentSqaure.on
-                    }
-                    newSquares.push(updatedSqaure)
-                }else{
-                    newSquares.push(currentSqaure)
-                }
-            }
-            return newSquares
+            return prevSquares.map((square)=>{
+                return square.id === id ? {...square, on: !square.on} : square
+            })
+            // const newSquares = []
+            // for (let i = 0; i < prevSquares.length; i++){
+            //     const currentSqaure = prevSquares[i]
+            //     if(currentSqaure.id === id){
+            //         const updatedSqaure = {
+            //             ...currentSqaure,
+            //             on: !currentSqaure.on
+            //         }
+            //         newSquares.push(updatedSqaure)
+            //     }else{
+            //         newSquares.push(currentSqaure)
+            //     }
+            // }
+            // return newSquares
         })
         // setOn(preOn => !preOn)
         console.log(id)
     }
 
-    const styles = {
-        backgroundColor: squares.on ? "#222222" : "#00D8FF"
-    }
+    // const styles = {
+    //     backgroundColor: squares.on ? "#222222" : "#00D8FF"
+    // }
  
     const squareElements = squares.map(square => (
-      <div style={styles} className="box" key={square.id} id={square.id} on={square.on} onClick={()=>toggle(square.id)}/>
+      <div style={{backgroundColor: square.on ? "#222222" : "#00D8FF"}} className="box" key={square.id} id={square.id} on={square.on} onClick={()=>toggle(square.id)}/>
     ))
 
     return (
