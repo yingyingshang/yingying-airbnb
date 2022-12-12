3ea646de254cd322dd1c9333496f69bd8e585af4
Brita Shang <brita.shang@activenetwork.com>
2022-12-12 21:10:44 +0800

    add select options

--- src/components/Form.js
+++ src/components/Form.js
@@ -8,7 +8,8 @@ export default function Form (){
             email: "", 
             comments: "", 
             isFriendly: true,
-            employment: ""
+            employment: "",
+            favColor: ""
         }
     )
 
@@ -100,6 +101,24 @@ export default function Form (){
                 />
                 <label htmlFor="full-time">Full-time</label>
                 <br />
+
+                <label htmlFor="favColor">What is your favorite color?</label>
+                <br />
+                <select
+                    id = "favColor"
+                    value = {formData.favColor}
+                    onChange = {handleChange}
+                    name = "favColor"
+                >
+                    <option value="">-- Choose --</option>
+                    <option value="red">Red</option>
+                    <option value="orange">Orange</option>
+                    <option value="yellow">Yellow</option>
+                    <option value="green">Green</option>
+                    <option value="blue">Blue</option>
+                    <option value="indigo">Indigo</option>
+                    <option value="violet">Violet</option>
+                </select> 
             </fieldset>
         </form>
     )
