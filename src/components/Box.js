9c6ab7ccb9a40a74bd6b48c35c7017db9df7e201
Brita Shang <brita.shang@activenetwork.com>
2022-12-12 13:38:16 +0800

    add setsquare

--- src/components/Box.js
+++ src/components/Box.js
@@ -3,17 +3,33 @@ import boxes from "./boxesData"
 
 export default function Box(){
 
-    const [squares] = React.useState(boxes)
+    const [squares, setSquares] = React.useState(boxes)
 
     const [on, setOn] = React.useState(squares.on)
 
     const styles = {
-        backgroundColor: on ? "#222222" : "transparent"
+        backgroundColor: on ? "#222222" : "#00D8FF"
     }
 
     function toggle(id){
-        setOn(preOn => !preOn)
-        console.log(id)
+        setSquares(prevSquares => {
+            const newSquares = []
+            for (let i = 0; i < prevSquares.length; i++){
+                const currentSqaure = prevSquares[i]
+                if(currentSqaure.id === id){
+                    const updatedSqaure = {
+                        ...currentSqaure,
+                        on: !currentSqaure.on
+                    }
+                    newSquares.push(updatedSqaure)
+                }else{
+                    newSquares.push(currentSqaure)
+                }
+            }
+            return newSquares
+        })
+        // setOn(preOn => !preOn)
+        // console.log(id)
     }
  
     const squareElements = squares.map(square => (
