829c88b6b293d5b2c4bc514de35eaa5ab2ef86c7
Brita Shang <brita.shang@activenetwork.com>
2022-12-13 17:01:06 +0800

    useeffect dependency array

--- src/components/GetAPI.js
+++ src/components/GetAPI.js
@@ -2,17 +2,23 @@ import React from "react";
 
 export default function GetAPI(){
     const [starWarsData, setStarWarsData] = React.useState({})
+    const [count, setCount] = React.useState(0)
 
     console.log("Component rendered")
 
     // side effects
+    // version 03
 
-    React.useEffect(function(){
-        fetch("https://swapi.dev/api/people/1")
-        .then(res => res.json())
-        .then(data => setStarWarsData(data))
-    })
+    React.useEffect(()=>{console.log("Effect function ran")}, [count])
 
+    // version 02
+    // React.useEffect(function(){
+    //     fetch("https://swapi.dev/api/people/1")
+    //     .then(res => res.json())
+    //     .then(data => setStarWarsData(data))
+    // })
+
+    // version 01
     // fetch("https://swapi.dev/api/people/1")
     //     .then(res => res.json())
     //     .then(data => setStarWarsData(data))
@@ -20,6 +26,8 @@ export default function GetAPI(){
     return (
         <div>
             <pre>{JSON.stringify(starWarsData, null, 2)}</pre>
+            <h2>The count is {count}</h2>
+            <button onClick={()=> setCount(preCount => preCount + 1)}>Add</button>
         </div>
     )
 }
